2ce411a0efa3af1b0d8b309883052d7c86f26f20
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-06 12:55:15 -0300

    adjustsinitial adjustments

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Olá vue!</h1>
+    <h1>Olá vue lafera!</h1>
   </div>
 </template>
 
@@ -11,5 +11,26 @@ export default {
 </script>
 
 <style>
+@font-face{
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+*{
+  font-family: "RobotoMono", monospace;
+}
 
+body{
+  margin: 0;
+}
+
+#app{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: white;
+  background: linear-gradient(to right, rgb(83,105,118),rgb(41,46,73));
+}
 </style>
--- calculator/src/main.js
+++ calculator/src/main.js
@@ -2,8 +2,5 @@ import Vue from 'vue'
 import App from './App'
 
 new Vue({
-    el: '#app',
-    render(createElement) {
-        return createElement(App)
-    }
-})
+    render: h => h(App)
+}).$mount("#app")
