a34f895bbb558cdf6093b61dedb12c782eabcf75
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-06 16:23:05 -0300

    display

--- calculator/src/components/Display.vue
+++ calculator/src/components/Display.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="display">
-    0
+    {{value}}
   </div>
 </template>
 
 <script>
 export default {
-
+  props:['value']
 }
 </script>
 
 <style>
-
+.display{
+  grid-column: span 4;
+  background-color: black;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 20px;
+  font-size: 2.1em;
+  overflow: hidden;
+}
 </style>
