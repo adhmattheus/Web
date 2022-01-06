4d67d4c7eb880539b12957efbb291a9eda8e95f3
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-06 17:00:27 -0300

    finished

--- calculator/src/main/Calculator.vue
+++ calculator/src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="calculator">
-    <Display value="1000"/>
+    <Display :value="displayValue"/>
     <Button label="AC" triple @onClick="clearMemory" />
     <Button label="/" operation @onClick="setOperation"/>
     <Button label="7" @onClick="addDigit"/>
@@ -26,32 +26,67 @@ import Display from "../components/Display"
 import Button from "../components/Button"
 
 export default {
+  data:function(){
+    return{
+      displayValue:"0",
+      clearDisplay:false,
+      operation:null,
+      values:[0,0],
+      current:0
+    }
+  },
   components:{Button,Display},
   methods:{
     clearMemory(){
-      console.log('Limpar Memória!')
+      Object.assign(this.$data, this.$options.data())
     },
     setOperation(operation){
-      console.log('Operação' + operation)
+      if(this.current ===0){
+        this.operation = operation
+        this.current = 1
+        this.clearDisplay = true
+      }else{
+        const equals = operation ===  "="
+        const currentOperation = this.operation
+
+        try{
+          this.values[0] = eval(
+           `${this.values[0]} ${currentOperation} ${this.values[1]}` 
+          )
+        }catch (e){
+          this.$emit('onError',e)
+        }
+        this.values[1] = 0
+
+        this.displayValue = this.values[0]
+        this.operation = equals ? null : operation
+        this.current = equals ? 0 : 1
+        this.clearDisplay = !equals
+      }
     },
     addDigit(n){
-      console.log('Digito ' + n)
+      if(n==="." && this.displayValue.includes(".")){
+        return
+      }
+      const clearDisplay = this.displayValue==="0" || this.clearDisplay
+      const currentValue = clearDisplay ? "" : this.displayValue
+      const displayValue = currentValue + n
+
+      this.displayValue = displayValue
+      this.clearDisplay = false
+      this.values[this.current] = displayValue
     }
   }
-
 }
 </script>
-
 <style>
 .calculator{
   height: 320px;
   width: 235px;
   border-radius: 5px;
   overflow: hidden;
-
   display: grid;
   grid-template-columns: repeat(4,25%);
   grid-template-rows: 1fr 48px 48px 48px 48px 48px;
-
 }
 </style>
