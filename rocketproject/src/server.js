27cced9dd58a5380fa68d585408d613246058963
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-15 13:50:00 -0300

    Alterações nos arquivos 'ejs'

--- rocketproject/src/server.js
+++ rocketproject/src/server.js
@@ -14,4 +14,4 @@ server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
-server.listen(3000, () => console.log("RODANDO"))
+server.listen(3000, () => console.log("LAFERA SERVER RUNNING SUCCESSFULLY"))
