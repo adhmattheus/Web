478056ed052c0832ad4b5687cfedd30a98fed6f0
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-15 19:10:40 -0300

    janela de mensagem modal

--- rocketproject/public/scripts/main.js
+++ rocketproject/public/scripts/main.js
@@ -1,15 +1,33 @@
 import Modal from './modal.js';
 
 const modal = Modal();
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector(".modal button");
+
 //pegar os botões com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check");
 
   checkButtons.forEach(button =>{
     //adicionar a escuta
-    button.addEventListener("click", event => {
-      //abrir modal
-      modal.open();
-    }); 
+    button.addEventListener("click", handleClick);
   });
 
-//pegar quando o "marcar como lido" for clicado
+//botao delete acionado
+  const deleteButton = document.querySelectorAll(".actions a.delete");
+
+    deleteButton.forEach(button => {
+      button.addEventListener("click", (event) => handleClick(event, false));
+    });
+
+    function handleClick(event, check = true){
+
+      const text  = check ? "Marcar como lida" : "Excluir" 
+
+      modalTitle.innerHTML = `${text} esta pergunta?`
+      modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
+      //abrir modal
+      modal.open();
+    };
+
+   
--- rocketproject/public/scripts/modal.js
+++ rocketproject/public/scripts/modal.js
@@ -1,11 +1,18 @@
 export default function Modal(){
+
+  const modalWrapper = document.querySelector('.modal-wrapper');
+  const cancelButton = document.querySelector('.button.cancel');
+
+  cancelButton.addEventListener("click",close);
+
+
   //ativa a classe active em modal
   function open(){
-    document.querySelector('.modal-wrapper').classList.add("active");
+    modalWrapper.classList.add("active");
   };
   //desativa a classe active em modal
   function close(){
-
+    modalWrapper.classList.remove("active")
   };
 
   return{
