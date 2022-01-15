e5f60159e5edd4142db46a3851e5da4030a0906b
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-15 11:40:18 -0300

    conexão do server finalizada

--- rocketproject/src/server.js
+++ rocketproject/src/server.js
@@ -6,8 +6,12 @@ const server = express()
 
 server.set('view engine', 'ejs')
 
+server.use(express.static("public"))
+
 server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
-server.listen(3000, () => console.log("SERVIDOR DO LAFERA RODANDO."))
+server.listen(3000, () => console.log("RODANDO"))
