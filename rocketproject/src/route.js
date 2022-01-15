135349464ec01db78195a6badff25f7b4bbe5b87
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-15 20:45:06 -0300

    recuperando dados e controles

--- rocketproject/src/route.js
+++ rocketproject/src/route.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const route = express.Router()
+const express = require('express');
+const route = express.Router();
+const QuestionController = require('./controllers/QuestionController');
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index"));
+route.get('/room', (req, res) => res.render("room"));
+route.get('/create-pass', (req, res) => res.render("create-pass"));
 
+route.post('/room/:room/:question/:action', QuestionController.index);
 
 module.exports = route;
--- rocketproject/src/server.js
+++ rocketproject/src/server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const route = require('./route')
-const path = require('path')
+const express = require('express');
+const route = require('./route');
+const path = require('path');
 
-const server = express()
+const server = express();
 
-server.set('view engine', 'ejs')
+server.set('view engine', 'ejs');
 
-server.use(express.static("public"))
+server.use(express.static("public"));
 
-server.set('views', path.join(__dirname, 'views'))
+server.set('views', path.join(__dirname, 'views'));
 
-server.use(express.urlencoded({extended: true}))
+server.use(express.urlencoded({extended: true}));
 
-server.use(route)
+server.use(route);
 
-server.listen(3000, () => console.log("LAFERA SERVER RUNNING SUCCESSFULLY"))
+server.listen(3000, () => console.log("LAFERA SERVER RUNNING SUCCESSFULLY"));
