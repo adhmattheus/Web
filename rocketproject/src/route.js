aecf9fdb936a9c34c138c75b18d67578a949c907
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-15 21:09:45 -0300

    organizando as rotas das telas

--- rocketproject/src/route.js
+++ rocketproject/src/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const route = express.Router();
 const QuestionController = require('./controllers/QuestionController');
 
-route.get('/', (req, res) => res.render("index"));
+const route = express.Router();
+
+route.get('/', (req, res) => res.render("index", {page:'enter-room'}));
+route.get('/create-pass', (req, res) => res.render("index", {page:'create-pass'}));
+
 route.get('/room', (req, res) => res.render("room"));
-route.get('/create-pass', (req, res) => res.render("create-pass"));
 
 route.post('/room/:room/:question/:action', QuestionController.index);
 
