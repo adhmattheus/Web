cf29fc4908a980c4d652a327aa87d516f5a73d02
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-17 13:04:58 -0300

    banco de dados finalizado

--- rocketproject/src/controllers/RoomController.js
+++ rocketproject/src/controllers/RoomController.js
@@ -1,10 +1,26 @@
+const Database = require("../db/config");
+
 module.exports = {
-  create(req, res){
+  async create(req, res){
+    const db = await Database();
+    const pass = req.body.password;
+    let roomId;
 
-    let roomId = 123456
+    for(var i=0;i<6;i++){
+      i==0 ? roomId = Math.floor(Math.random()*10).toString() : roomId += Math.floor(Math.random()*10).toString();
+    };
 
-    
-    
-    res.redirect(`/room/{roomId}`)
-  }
+
+    await db.run(`INSERT INTO rooms (
+                  id,
+                  pass
+                ) VAlUES (
+                  ${parseInt(roomId)},
+                  "${pass}"
+                )`)
+
+                await db.close()
+
+                res.redirect(`/room/${roomId}`)
+    }
 }
