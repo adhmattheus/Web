62d37b876084cce5836c5c859b0362926c903d52
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-01-18 11:59:15 -0300

    marcando como lida e excluindo perguntas da sala

--- rocketproject/src/controllers/RoomController.js
+++ rocketproject/src/controllers/RoomController.js
@@ -25,15 +25,18 @@ module.exports = {
                           ${parseInt(roomId)},
                           "${pass}"
                         )`)
-        }
+          }
     }  
                 await db.close();
                 res.redirect(`/room/${roomId}`);
     },
 
-    open(req, res){
-
+    async open(req, res){
+      const db = await Database();
       const roomId = req.params.room
-      res.render("room", {roomId: roomId})
+      const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+      const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read= 1`)
+      
+      res.render("room", {roomId: roomId, questions:questions, questionsRead:questionsRead})
     }
 }
--- rocketproject/src/db/init.js
+++ rocketproject/src/db/init.js
@@ -1,21 +1,23 @@
 const Database = require("./config")
 
 const initDb = {
-  async init(){
-    const db = await Database()
+    async init(){
+        const db = await Database()
 
-    await db.exec(`CREATE TABLE rooms(
-      id INTEGER PRIMARY KEY,
-      pass TEXT      
-    )`);
+        await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`);
 
-    await db.exec(`CREATE TABLE questions(
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      titulo TEXT,
-      read INT
-    )`);
-    
-    await db.close()
-  }
+        await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT,
+            room INT
+        )`);
+
+        await db.close()
+    }
 }
+
 initDb.init();
