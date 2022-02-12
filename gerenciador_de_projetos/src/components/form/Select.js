72942595cd1362a6cd3dc1aaf2ee06c376365127
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-02-12 18:10:01 -0300

    Inserindo dados no banco via API

--- gerenciador_de_projetos/src/components/form/Select.js
+++ gerenciador_de_projetos/src/components/form/Select.js
@@ -1,23 +1,22 @@
 import styles from './Select.module.css'
 
-
 function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
-
       <label htmlFor={name}>{text}:</label>
-
-      <select name={name} id={name}>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ''}
+      >
         <option>Selecione uma opção</option>
-
-        {options.map((option) => (
-          <option value={options.id} hey={options.id}>
-            {option.name}
+        {options.map((options) => (
+          <option value={options.id} key={options.id}>
+            {options.name}
           </option>
         ))}
-
       </select>
-
     </div>
   )
 }
