5d9b26f217c22a47b5f4c0b165ff9179fb9d7677
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-02-12 19:47:45 -0300

    mensagens do sistemas

---
+++ gerenciador_de_projetos/src/components/layout/Message.js
@@ -0,0 +1,8 @@
+import styles from './Message.module.css'
+
+function Message({ type, msg }) {
+  
+  return <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+
+}
+export default Message
