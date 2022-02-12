28a1287c2e1d0b7f07faf461cb6ffcb19455e8c4
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-02-12 20:06:05 -0300

    configurando as messagens

--- gerenciador_de_projetos/src/components/pages/Projects.js
+++ gerenciador_de_projetos/src/components/pages/Projects.js
@@ -1,10 +1,18 @@
+import { useLocation } from 'react-router-dom'
 import Message from '../layout/Message'
 
 function Projects() {
+
+  const location = useLocation()
+  let message = ''
+  if (location.state) {
+    message = location.state.message
+  }
+
   return (
     <div>
       <h1>Meus Projetos</h1>
-      <Message msg="Alguma mensagem"/>
+      {message && <Message type="success" msg={message} />}
     </div>
   )
 }
