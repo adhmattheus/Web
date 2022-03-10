b93b07f98671c247c833639b16590501aa723fb0
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-03-10 09:11:37 -0300

    componente summary

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,3 +1,4 @@
+import { Dashbord } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
@@ -5,6 +6,7 @@ export function App() {
   return (
     <>
       <Header />
+      <Dashbord />
       <GlobalStyle />
     </>
   );
---
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary";
+import { Container } from "./styles";
+
+export function Dashbord() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+}
