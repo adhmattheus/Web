79ace7700ab14a8e3b237c233a1f7876db847bcf
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-04-18 12:56:58 -0300

    estilos globais

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/home.module.scss';
+
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello world
       </h1>
     </>
