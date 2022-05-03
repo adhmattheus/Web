3eecfe0c6cb3ece486fc91e1a7f3fa0bc6ff97b4
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-05-03 20:29:05 -0300

    Gerando sessão de checkout

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { signIn, useSession } from 'next-auth/react';
+import { stripe } from '../../services/stripe';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +7,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+    
+  }
+
+
   return (
     <button
       type="button"
       className={styles.subscibeButton}
+      onClick={handleSubscribe}
     >
       subscribe now
     </button>
---
+++ ignews/src/pages/api/auth/subscribe.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react"
+import { stripe } from "../../../services/stripe";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+  if (req.method === 'POST') {
+    const session = await getSession({ req })
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+
+    })
+
+
+    const stripeChekoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: 'price_1Kqda0GtJvv8h2m9AS8hOB6k', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    })
+
+    return res.status(200).json({ sessionId: stripeChekoutSession.id })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
+}
