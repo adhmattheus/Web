beb2e57f4cf06e2fff577435318aa583e2c3a676
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-05-16 23:22:27 -0300

    componentes da pag de agendamento

--- srca/src/components/pages/Agendamento/index.jsx
+++ srca/src/components/pages/Agendamento/index.jsx
@@ -1,25 +1,21 @@
 import Select from 'react-select'
-
-import { Container, Content } from './styles'
+import { Container, Agenda, Calendario } from './styles'
 
 const atendimento = [
   { value: 'entrega de diploma', label: 'entrega de diploma' },
   { value: 'matrícula', label: 'matrícula' },
   { value: 'informações', label: 'informações' },
 ]
-
 const setor = [
   { value: 'SRCA', label: 'SRA' },
   { value: 'PROEN', label: 'PROEN' },
   { value: 'PROAE', label: 'PROAE' },
 ]
-
 const campus = [
   { value: 'Petrolina', label: 'Petrolina' },
   { value: 'Juazeiro', label: 'Juazeiro' },
   { value: 'Paulo Afonso', label: 'Paulo Afonso' },
 ]
-
 const horarios = [
   { value: '18:00', label: 'entrega de diploma' },
   { value: 'matrícula', label: 'matrícula' },
@@ -28,14 +24,20 @@ const horarios = [
 export function Agendamento() {
   return (
     <Container>
-      <h1>Faça seu agendamento.</h1>
-      <Content>
+
+
+
+      <Agenda>
+        <h1>Faça seu agendamento.</h1>
         <Select options={atendimento} />
         <Select options={setor} />
         <Select options={campus} />
         <Select options={horarios} />
-      </Content>
-      <div>ok</div>
+      </Agenda>
+
+      <Calendario>
+
+      </Calendario>
     </Container>
   )
 }
--- srca/src/components/pages/Agendamento/styles.js
+++ srca/src/components/pages/Agendamento/styles.js
@@ -1,8 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
- border: 2px solid black;
+export const Container = styled.main`
+border: 2px solid black;
+
+max-width: 1120px;
+margin: 60px auto;
+display: flex;
 `;
-export const Content = styled.div`
-  s
+export const Agenda = styled.div`
+  flex:1;
 `;
+export const Calendario = styled.div`
+  width: 380px;
+  background-color: red;
+  width: 50%;
+  height: 50;
+`;
+
+
