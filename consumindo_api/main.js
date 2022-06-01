178a17adafa13fc4b21c987b1f8c4b2db632cc70
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-06-01 10:45:02 -0300

    api post

--- consumindo_api/main.js
+++ consumindo_api/main.js
@@ -1,4 +1,9 @@
 const url = "http://localhost:5500/api"
+const newUser = {
+  name: "Lafera Adh",
+  avatar: "https://cdn1.iconfinder.com/data/icons/people-avatars-23/24/people_avatar_head_wolverine_logan_xman_marvel-512.png",
+  city: "Cabrobó"
+}
 
 function getUser() {
   axios.get(url)
@@ -8,5 +13,13 @@ function getUser() {
     })
     .catch(error => console.log(error))
 }
-
 getUser();
+
+function addNewUser() {
+  axios.post(url, newUser)
+    .then(response => {
+      alert(JSON.stringify(response.data))
+    })
+    .catch(error => console.log(error))
+}
+addNewUser();
