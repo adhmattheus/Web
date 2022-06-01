995c24f1926d6c7efc0bd6fc9c54b126c2423ee3
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-06-01 09:45:56 -0300

    Criando arquivo html e axios cdn

--- srca/src/components/pages/Agendamento/index.jsx
+++ srca/src/components/pages/Agendamento/index.jsx
@@ -3,6 +3,7 @@ import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
 import { Container, Agenda, Calendario, ContentBotao } from './styles'
+import { useState } from 'react';
 
 const atendimento = [
   { value: 'entrega de diploma', label: 'entrega de diploma' },
@@ -20,12 +21,37 @@ const campus = [
   { value: 'Paulo Afonso', label: 'Paulo Afonso' },
 ]
 const horarios = [
-  { value: '18:00', label: 'entrega de diploma' },
-  { value: 'matrícula', label: 'matrícula' },
-  { value: 'informações', label: 'informações' },
+  { value: '10:00', label: '10:00' },
+  { value: '10:30', label: '10:30' },
+  { value: '18:00', label: '18:00' },
 ]
 
 export function Agendamento() {
+
+  const [agendamento, setAgendamento] = useState({
+    atendimento : '',
+    setor : '',
+    campus : ''
+  });
+
+  const onChangeAgendamento = e => 
+    setAgendamento({...agendamento,[e.target.agendamento.atendimento] : e.target.value });
+  
+
+  const Submit = (e) => {
+    e.preventDefault();
+    console.log({ agendamento });
+  }
+
+  // const data = {
+  //     atendimento:
+  //     setor:
+  //     campus: 
+  //     horario:
+  //     dia:
+  // }
+
+
   return (
     <>
       <Container>
@@ -33,28 +59,52 @@ export function Agendamento() {
         <Agenda>
           <h1>Faça seu agendamento</h1>
 
-          <label>Atendimento</label>
-          <Select options={atendimento} />
+          <form onSubmit={Submit}>
+            <label>
+              Atendimento
+              <select value={agendamento.atendimento} onChange={onChangeAgendamento}>
 
-          <label>Setor</label>
-          <Select options={setor} />
+                <option value="Matrícula">Matrícula</option>
+                <option value="Entrega de diploma">Entrega de diploma</option>
 
-          <label>Campus</label>
-          <Select options={campus} />
+              </select>
+            </label>
 
-          <label>Horários</label>
-          <Select options={horarios} />
+            <label>
+              Setor
+              <select value={agendamento.setor} onChange={onChangeAgendamento}>
+
+                <option value="Proae">Proae</option>
+                <option value="SRCA">SRCA</option>
+
+              </select>
+            </label>
+
+            <label>
+              Campus
+              <select value={agendamento.campus} onChange={onChangeAgendamento}>
+
+                <option value="Petrolina">Petrolina</option>
+                <option value="Juazeiro">Juazeiro</option>
+                <option value="Paulo Afonso">Paulo Afonso</option>
+
+              </select>
+            </label>
+
+
+          </form>
         </Agenda>
 
         <Calendario>
-          <DayPicker/>
+          <DayPicker />
         </Calendario>
 
       </Container>
 
       <ContentBotao>
 
-        <button type="submit">Agendar</button>
+        <button type="submit" onClick={(Submit)}>Agendar</button>
+
         <button type="submit">Cancelar</button>
 
       </ContentBotao>
